d5a14c7417202fbe3fb483c744e07ee636efacef
Chris Knight <cknight1234@gmail.com>
2021-07-27 11:14:48 +0100

    feat: jsx demo

--- mod.ts
+++ mod.ts
@@ -22,9 +22,27 @@
 //     event.respondWith(response);
 //   });
 
+import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
+import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Hello from JSX</title>
+      </head>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+    </html>
+  );
+}
+
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World 2!", {
-    headers: { "content-type": "text/plain" },
+  // renderToString generates html string from JSX components.
+  const response = new Response(renderToString(<App />), {
+    headers: { "content-type": "text/html; charset=utf-8" },
   });
+
   event.respondWith(response);
 });
