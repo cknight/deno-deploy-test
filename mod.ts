c5f884758d05f5101ea30d5de4cb7429551fd4d0
Chris Knight <cknight1234@gmail.com>
2021-07-27 10:55:10 +0100

    feat: hello world

---
+++ mod.ts
@@ -0,0 +1,30 @@
+// import {
+//   Level,
+//   Logger,
+//   LogRecord,
+//   ConsoleStream,
+//   TimeUnit,
+// } from "../optic/mod.ts";
+
+// const log = new Logger().addStream(new ConsoleStream());
+// //import { blue } from "https://deno.land/std@0.92.0/fmt/colors.ts";
+
+// //const str:string[] = (Deno as {args?: string[]}).args ?? [];
+
+// addEventListener("fetch", (event) => {
+//     //log.info(event.request);
+//     //console.log(event.request.method);
+//     const response = new Response("Hello World! " + event.request.method, {
+//       headers: { "content-type": "text/plain" },
+//     });
+//     //console.log(event.request, event.request.headers, event.request.method);
+//     log.info('Headers:', event.request.headers);
+//     event.respondWith(response);
+//   });
+
+addEventListener("fetch", (event) => {
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
+});
