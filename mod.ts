b5f7335efb94506466e7255fdda65702bb8aa5c0
Chris Knight <cknight1234@gmail.com>
2021-07-27 20:13:41 +0100

    feat: update 3

--- mod.ts
+++ mod.ts
@@ -22,27 +22,9 @@
 //     event.respondWith(response);
 //   });
 
-import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
-import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
-
-function App() {
-  return (
-    <html>
-      <head>
-        <title>Hello from JSX</title>
-      </head>
-      <body>
-        <h1>Hello world</h1>
-      </body>
-    </html>
-  );
-}
-
 addEventListener("fetch", (event) => {
-  // renderToString generates html string from JSX components.
-  const response = new Response(renderToString(<App />), {
-    headers: { "content-type": "text/html; charset=utf-8" },
+  const response = new Response("Hello World 3!", {
+    headers: { "content-type": "text/plain" },
   });
-
   event.respondWith(response);
 });
