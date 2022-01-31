c6e9a53c8422605e63f837f02113d48f2a39590a
Chris Knight <cknight1234@gmail.com>
2022-01-31 09:41:03 +0000

    break: intentionally break build to test Deploy

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-console.log(Deno.memoryUsage);
+console.log(Deno.memoryUsage().heapUsed);
 console.log("Listening on http://localhost:8000");
 serve((_req: Request) => {
   return new Response("Hello World!", {
